897b12079ca3ad8dc128afcb03e5b17d3acf54d9
ball <138590557+SarayutZ@users.noreply.github.com>
2023-08-09 08:59:44 +0700

    version ใกล้ สมบูรณ์ v1
    ใกล้ความจริงละ

--- java.js
+++ java.js
@@ -2,12 +2,12 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     const products = [
-      { id: 1, price: 250 },
-      { id: 2, price: 260 },
-      { id: 3, price: 175 },
-      { id: 4, price: 185 },
-      { id: 5, price: 165 },
-      { id: 6, price: 250 },
+      { id: 1, price: 250 ,name:"จิตวิทยาสายดาร์ก"},
+      { id: 2, price: 260, name:"ถึงโมโหก็อย่าสู้กับคนโง่"},
+      { id: 3, price: 175,name:"เมื่อแมวที่บ้านคุณผันตัวมาเป็นไลฟ์โค้ช" },
+      { id: 4, price: 185,name:"ช่างหัวคุณสิครับ!"},
+      { id: 5, price: 165 ,name:"ป่านนี้เขานั่งกินไอติมสบายใจเฉิบไปแล้ว"},
+      { id: 6, price: 250 ,name:"เมื่อเส้นทางการทำงานโรยไปด้วยเปลือกทุเรียน"},
 
       // Add more products here if needed
     ];
@@ -37,16 +37,45 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   
     function updateTotal() {
-      let grandTotal = 0;
-      for (const productId in cart) {
-        const quantity = cart[productId];
-        const product = products.find(item => item.id === parseInt(productId));
-        if (product) {
-          grandTotal += product.price * quantity;
+        let grandTotal = 0;
+        let billDetails = "";
+      
+        for (const productId in cart) {
+          const quantity = cart[productId];
+          const product = products.find(item => item.id === parseInt(productId));
+          if (product) {
+            grandTotal += product.price * quantity;
+            billDetails += `ชื่อหนังสือ: ${product.name}\nจำนวน: ${quantity} เล่ม\nราคา: ${product.price * quantity} บาท\n\n`;
+          }
         }
+      
+        document.getElementById("billDetails").textContent = billDetails;
+        document.getElementById("grandTotal").textContent = grandTotal;
       }
-  
-      document.getElementById("grandTotal").textContent = grandTotal;
-    }
+      
   });
-  
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
